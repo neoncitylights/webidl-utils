183f5051b399fc66979fb5094c92143fb50c3cd3
Samantha <contact@samanthanguyen.me>
2024-06-27 17:26:20 -0500

    docs: adjust crate summary (#8)
    also add maintenance status badge to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-/*!
-A crate of utilities for working with the WebIDL AST by extending `weedle2`
-*/
+//! A crate of utilities for working with the WebIDL AST by extending `weedle2`.
+//! The crate's API is not currently stable yet.
 
 mod extend;
 mod symbol;
