f6546f93488485fd4acb93b8d1dbf2afb73cc375
Samantha Nguyen <contact@samanthanguyen.me>
2024-06-26 21:54:25 -0500

    revert changes in docstring

--- src/symbol/with_docstring.rs
+++ src/symbol/with_docstring.rs
@@ -5,14 +5,14 @@ use weedle::*;
 
 /// A WebIDL symbol that may have a documentation string
 pub trait SymbolWithDocString {
-	fn docstring(&self) -> Option<weedle::common::Docstring>;
+	fn docstring(self) -> Option<weedle::common::Docstring>;
 }
 
 macro_rules! impl_symbol_with_docstring {
 	($($sym:ident),+ $(,)?) => {
 		$(
 			impl<'a> SymbolWithDocString for $sym<'a> {
-				fn docstring(&self) -> Option<weedle::common::Docstring> {
+				fn docstring(self) -> Option<weedle::common::Docstring> {
 					self.docstring
 				}
 			}
