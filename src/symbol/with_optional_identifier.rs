3da4282e3b914e224f55f76190a5de76d7a106c6
Samantha <contact@samanthanguyen.me>
2024-06-26 21:56:08 -0500

    api: use self reference for symbol methods where possible (#4)

--- src/symbol/with_optional_identifier.rs
+++ src/symbol/with_optional_identifier.rs
@@ -4,14 +4,14 @@ use weedle::namespace::OperationNamespaceMember;
 
 /// A WebIDL symbol that may have an identifier
 pub trait SymbolWithOptionalIdentifier<'a> {
-	fn identifier(self) -> Option<weedle::common::Identifier<'a>>;
+	fn identifier(&self) -> Option<weedle::common::Identifier<'a>>;
 }
 
 macro_rules! impl_symbol_with_optional_identifier {
 	($($sym:ident),+ $(,)?) => {
 		$(
 			impl<'a> SymbolWithOptionalIdentifier<'a> for $sym<'a> {
-				fn identifier(self) -> Option<weedle::common::Identifier<'a>> {
+				fn identifier(&self) -> Option<weedle::common::Identifier<'a>> {
 					self.identifier
 				}
 			}
--- src/symbol/with_readonly.rs
+++ src/symbol/with_readonly.rs
@@ -3,14 +3,14 @@ use weedle::mixin::AttributeMixinMember;
 
 /// A WebIDL symbol that may have a readonly modifier
 pub trait SymbolWithReadOnly {
-	fn readonly(self) -> Option<weedle::term::ReadOnly>;
+	fn readonly(&self) -> Option<weedle::term::ReadOnly>;
 }
 
 macro_rules! impl_symbol_with_readonly {
 	($($sym:ident),+ $(,)?) => {
 		$(
 			impl<'a> SymbolWithReadOnly for $sym<'a> {
-				fn readonly(self) -> Option<weedle::term::ReadOnly> {
+				fn readonly(&self) -> Option<weedle::term::ReadOnly> {
 					self.readonly
 				}
 			}
