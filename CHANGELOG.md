766404b7ecc3006e209a44d644f363fee6171662
Samantha Nguyen <contact@samanthanguyen.me>
2024-06-28 17:03:59 -0500

    feat: add `ExtendRecordKeyType` trait

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.1.1 (Unreleased / YYYY-MM-DD)
+
+- Add and implement `ExtendRecordKeyType` trait for `weedle::types::RecordKeyType`
+
 ## 0.1.0 (2024-06-27)
 
 - Initial release of the `webidl-utils` library
--- src/extend/types.rs
+++ src/extend/types.rs
@@ -261,6 +261,32 @@ impl<'a> ExtendRecordType<'a> for RecordType<'a> {
 	}
 }
 
+/// Extension methods for `RecordKeyType`
+pub trait ExtendRecordKeyType<'a> {
+	fn byte() -> Self;
+	fn dom() -> Self;
+	fn usv() -> Self;
+	fn non_any(nat: NonAnyType<'a>) -> Self;
+}
+
+impl<'a> ExtendRecordKeyType<'a> for RecordKeyType<'a> {
+	fn byte() -> Self {
+		Self::Byte(ByteString)
+	}
+
+	fn dom() -> Self {
+		Self::DOM(DOMString)
+	}
+
+	fn usv() -> Self {
+		Self::USV(USVString)
+	}
+
+	fn non_any(nat: NonAnyType<'a>) -> Self {
+		Self::NonAny(nat)
+	}
+}
+
 /// Extension methods for `Type`
 pub trait ExtendType {
 	fn single_any() -> Self;
@@ -408,15 +434,14 @@ mod extend_frozen_array {
 
 #[cfg(test)]
 mod extend_record {
-	use crate::{ExtendRecordType, ExtendType};
-	use weedle::term::ByteString;
+	use crate::{ExtendRecordKeyType, ExtendRecordType, ExtendType};
 	use weedle::types::{RecordKeyType, RecordType, Type};
 
 	#[test]
 	fn test_new() {
-		let r = RecordType::new(RecordKeyType::Byte(ByteString), Type::single_any());
+		let r = RecordType::new(RecordKeyType::byte(), Type::single_any());
 
-		assert_eq!(*(r.generics.body.0), RecordKeyType::Byte(ByteString));
+		assert_eq!(*(r.generics.body.0), RecordKeyType::byte());
 		assert_eq!(*(r.generics.body.2), Type::single_any());
 	}
 }
